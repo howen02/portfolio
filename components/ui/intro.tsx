17791980b9d0cd66fcdf7fb35bd373114557ea6e
yaphowen <e0985925@u.nus.edu>
2024-02-27 21:44:30 +0800

    Create intro branch

---
+++ components/ui/intro.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Intro = () => {
+    return (
+        <div className="rounded-xl bg-white m-4 p-4 w-10">
+            <h2 className="text-xl font-bold mb-2">Who am I?</h2>
+            <p>
+                I'm currently a second-year Computer Science student studying at
+                the National University of Singapore.
+            </p>
+            <p>
+                You can also catch me exploring Web3 and trading NFTs on the
+                side if not watching my favourite Anime
+            </p>
+        </div>
+    );
+};
+
+export default Intro;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,16 @@
 "use client";
 
+import Intro from "components/ui/intro";
 import React from "react";
 
 export default function Home() {
     return (
-        <div className=" w-full dark:bg-black bg-white  dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative flex items-center justify-center">
+        <div className="w-full dark:bg-black bg-white  dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative flex items-center justify-center">
             <div className="h-[50rem]">
                 <p className="text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text bg-gradient-to-b py-8">
                     Hi, I'm <span className="text-blue-500">Howen</span>
                 </p>
+                <Intro />
             </div>
         </div>
     );
